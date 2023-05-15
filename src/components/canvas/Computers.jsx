bd54cc58e6e1235e04e81505c36596388bf8a3de
Yash Kaskar <yash.kaskar99@gmail.com>
2023-05-15 08:50:35 +0530

    hero and computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from './../Loader';
 
 const Computers = ({isMobile}) => {
 
-  const computer = useGLTF('./vintage/scene.gltf')
+  const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
     <mesh>
@@ -21,7 +21,7 @@ const Computers = ({isMobile}) => {
       />
       <primitive object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={ isMobile ? [0, -3, -2.2]:[0, -3.25, -1.5]}
+        position={ isMobile ? [0, -4.75, -2.2]:[0, -5.12, -2.25]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
