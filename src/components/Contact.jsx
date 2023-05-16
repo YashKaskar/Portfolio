ada823c54ae44c1d6d6f5b8c1d9d25daf5a00986
Yash Kaskar <yash.kaskar99@gmail.com>
2023-05-16 13:58:17 +0530

    updated contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_p2lw3ss',
+        'template_et1zk3c',
         {
           from_name: form.name,
           to_name: "Yash Kaskar",
@@ -42,7 +42,8 @@ const Contact = () => {
           to_email: "yash.kaskar99@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'O_sIiEvsxtcBmOO6N'
+        
       )
       .then(
         () => {
@@ -58,11 +59,11 @@ const Contact = () => {
         (error) => {
           setLoading(false);
           console.error(error);
-
           alert("Ahh, something went wrong. Please try again.");
         }
       );
   };
+
 
   return (
     <div
